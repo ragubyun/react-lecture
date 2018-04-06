af967ddeab86254d6b91e15e041150ac46b9f090
Rain Byun <ragubyun@gmail.com>
2018-04-06 13:58:03 +0900

    [Day 5] Router:  passing props

--- ch09/router-test/src/components/App.js
+++ ch09/router-test/src/components/App.js
@@ -9,14 +9,28 @@ import SongList from './SongList';
 import Members from './Members';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            members : [
+                { name: 'Maggie Adams', photo: 'photos/Mag.png' },
+                { name: 'Sammie Purcell', photo: 'photos/Sam.png' },
+                { name: 'Tim Purcell', photo: 'photos/Tim.png' },
+                { name: 'Scott King', photo: 'photos/King.png' },
+                { name: 'Johnny Pike', photo: 'photos/JPike.jpg' },
+                { name: 'Toby Ruckert', photo: 'photos/Toby.jpg' },
+            ]
+        }
+    }
+
     render() {
         return (
             <Router>
                 <div className="container">
                     <Header />
                     <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About}/>
-                    <Route path="/members" component={Members} />
+                    <Route path="/about" render={props => <About {...props} title="여우와 늙은이들" />} />
+                    <Route path="/members" render={props => <Members {...props} members={this.state.members} />} />
                     <Route path="/songs" component={SongList} />
                 </div>
             </Router>
